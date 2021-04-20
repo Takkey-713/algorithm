e82824fc138a74a37bbb41c2e744d6a9a7330f0d
Takkey-713 <takiyu713@gmail.com>
2021-04-20 13:05:11 +0900

    線形探索

--- linerSearch.js
+++ linerSearch.js
@@ -1,10 +1,16 @@
+const readlineSync = require("readline-sync");
 const linerSearch = (arg, num) => {
+  if (!arg.includes(num)) {
+    return `${num}は存在しません。`;
+  }
   for (i = 0; i < arg.length; i++) {
-    if 
+    if (arg[i] == num) {
+      return `${num}は${i + 1}番目に存在します。`;
+    }
   }
 };
 
 const array = [311, 314, 33, 12, 3, 97, 44, 25, 8];
 console.log(array, "\n探索したい数字を入力してください。");
 const num = readlineSync.question();
-linerSearch(array, num);
+console.log(linerSearch(array, Number(num)));
